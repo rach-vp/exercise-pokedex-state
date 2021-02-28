720973d07842ed3857fbf7dda6ffc999c75d718f
Raquel Picanço <picanco.raquel@gmail.com>
2021-02-28 09:39:16 -0400

    Refactor elements rendering to slider

--- src/App.js
+++ src/App.js
@@ -2,15 +2,39 @@ import React, { Component } from "react";
 import "./App.css";
 import Pokedex from "./Components/Pokedex";
 import PokedexPromise from 'pokedex-promise-v2';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.pokedex = new PokedexPromise();
     this.state = {
-      loading: false,
+      loading: true,
       pokemons: [],
+      currentIndex: 0,
+      length: 0,
     }
+    this.prevCard = this.prevCard.bind(this);
+    this.nextCard = this.nextCard.bind(this);
+    this.getFilteredList = this.getFilteredList.bind(this);
+  }
+
+  prevCard() {
+    const { currentIndex: current, length } = this.state;
+    this.setState(() => {
+      return current === 0
+      ? { currentIndex: length - 1 }
+      : { currentIndex: current - 1 };
+    });
+  }
+
+  nextCard() {
+    const { currentIndex: current, length } = this.state;
+    this.setState(() => {
+      return current === length - 1
+      ? { currentIndex: 0 }
+      : { currentIndex: current + 1 };
+    });
   }
 
   async componentDidMount() {
@@ -18,13 +42,28 @@ class App extends Component {
       .getPokemonsList()
       .then(res => res.results)
       .catch(err => err);
-    this.setState({ pokemons, loading: true });
+    this.setState({ pokemons, length: pokemons.length, loading: false });
+  }
+
+  getFilteredList() {
+    const { pokemons, currentIndex } = this.state;
+    return pokemons.filter((_pokemon, index) => index === currentIndex);
   }
 
   render() {
-    const { pokemons } = this.state;
+    const { loading } = this.state;
 
-    return <Pokedex pokemons={ pokemons } />;
+    return (
+      loading
+      ? <div>LOADING...</div>
+      : (
+          <main>
+            <FaArrowAltCircleLeft className="left-arrow" onClick={ this.prevCard }/>
+            <FaArrowAltCircleRight className="right-arrow" onClick={ this.nextCard } />
+            <Pokedex pokemons={ this.getFilteredList() } />
+          </main>
+        )
+    );
   }
 }
 
--- src/Components/Pokedex/Pokedex.jsx
+++ src/Components/Pokedex/Pokedex.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Pokemon from '../Pokemon';
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 import './style.css';
 
 
@@ -12,67 +11,25 @@ export default class Pokedex extends Component {
       currentIndex: 0,
       length: 0,
     };
-    this.nextCard = this.nextCard.bind(this);
-    this.prevCard = this.prevCard.bind(this);
-  }
-
-  prevCard() {
-    const { currentIndex: current, length } = this.state;
-    this.setState(() => {
-      return current === 0
-      ? { currentIndex: length - 1 }
-      : { currentIndex: current - 1 };
-    });
-  }
-
-  nextCard() {
-    const { currentIndex: current, length } = this.state;
-    this.setState(() => {
-      return current === length - 1
-      ? { currentIndex: 0 }
-      : { currentIndex: current + 1 };
-    });
-  }
-
-  componentDidUpdate(prevProps) {
-    const { pokemons } = this.props;
-    if(pokemons !== prevProps.pokemons) {
-      this.setState({ length: pokemons.length });
-    }
   }
 
   render() {
     const { pokemons } = this.props;
-    const { currentIndex } = this.state;
 
     return (
-      <main>
-        <FaArrowAltCircleLeft className="left-arrow" onClick={ this.prevCard }/>
-        <FaArrowAltCircleRight className="right-arrow" onClick={ this.nextCard } />
+      <section>
         {
-          pokemons.map(({ name, type, averageWeight, image }, index) => {
-            return index === currentIndex
-              ? <Pokemon
-                  key={name}
-                  name={name}
-                  type={type}
-                  averageWeight={averageWeight}
-                  image={image}
-                  className="visible"
-                />
-              : <Pokemon
-                  key={name}
-                  name={name}
-                  type={type}
-                  averageWeight={averageWeight}
-                  image={image}
-                  className="hidden"
-                />
-            }
+          pokemons.map(({ name, type, averageWeight, image }) =>
+            <Pokemon
+              key={name}
+              name={name}
+              type={type}
+              averageWeight={averageWeight}
+              image={image}
+            />
           )
         }
-        {/* <FaArrowAltCircleRight className="right-arrow" onClick={ this.nextCard } /> */}
-      </main>
+      </section>
     );
   }
 }
